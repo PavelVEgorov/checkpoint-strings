cd1eca0c93efbbb74a30c1fb6d5cbee25e71cb3f
Pavel Egorov <egorovpvspb@gmail.com>
2022-05-23 10:58:38 +0300

    tests done

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+function countVowels(text) {
+  const vowels = text.match(/[уУеЕёЁыЫаАоОэЭяЯиИюЮ]/g);
+  return vowels.length;
+}
+
+function splitWords(text) {
+  return text.match(/[а-яА-я]+/gi);
+}
+
+function removePunctuation(text) {
+  return text.match(/[а-яА-я]/gmi).join('');
+}
 
 module.exports = {
   countVowels,
